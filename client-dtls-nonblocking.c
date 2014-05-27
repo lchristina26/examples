39cfc63dbdb6dfe7cfafcea5dc7a111a388348b4
Leah T <leah@wolfssl.com>
2014-05-27 10:37:34 -0600

    more updates to nonblocking

--- client-dtls-nonblocking.c
+++ client-dtls-nonblocking.c
@@ -15,10 +15,10 @@
 #define SERV_PORT 	11111 
 
 enum {
-          TEST_SELECT_FAIL,
-          TEST_TIMEOUT,
-          TEST_RECV_READY,
-          TEST_ERROR_READY
+    TEST_SELECT_FAIL,
+    TEST_TIMEOUT,
+    TEST_RECV_READY,
+    TEST_ERROR_READY
 };
 
 void err_sys (const char* x) {
@@ -33,9 +33,9 @@ void sig_handler (const int sig) {
 }
 
 /* tcp select using dtls nonblocking function*/
-static int dnb_select(int socketfd, int to_sec)
+static int dtls_select(int socketfd, int to_sec)
 {
-   
+
     fd_set 	   recvfds, errfds;
     int 	   nfds = socketfd +1;
     struct timeval timeout = { (to_sec > 0) ? to_sec : 0, 0};
@@ -47,7 +47,7 @@ static int dnb_select(int socketfd, int to_sec)
     FD_SET(socketfd, &errfds);
 
     result = select(nfds, &recvfds, NULL, &errfds, &timeout);
-    
+
     if (result == 0)
 	return TEST_TIMEOUT;
     else if (result > 0) {
@@ -67,16 +67,16 @@ static void NonBlockingDTLS_Connect(CYASSL* ssl)
     int	     sockfd = (int)CyaSSL_get_fd(ssl); 
     int      select_ret;
     while (ret != SSL_SUCCESS && (error == SSL_ERROR_WANT_READ || 
-				 error == SSL_ERROR_WANT_WRITE)) {
-        int currTimeout = 1;
+		error == SSL_ERROR_WANT_WRITE)) {
+	int currTimeout = 1;
 	if (error == SSL_ERROR_WANT_READ)
-	   printf("... client would read block\n");
+	    printf("... client would read block\n");
 	else
 	    printf("... client would write block\n");
-    	currTimeout = CyaSSL_dtls_get_current_timeout(ssl);
-	select_ret = dnb_select(sockfd, currTimeout);
+	currTimeout = CyaSSL_dtls_get_current_timeout(ssl);
+	select_ret = dtls_select(sockfd, currTimeout);
 	if ( ( select_ret == TEST_RECV_READY) || 
-					(select_ret == TEST_ERROR_READY)) {	
+		(select_ret == TEST_ERROR_READY)) {	
 	    ret = CyaSSL_connect(ssl);
 	    error = CyaSSL_get_error(ssl, 0);
 	}
@@ -84,7 +84,7 @@ static void NonBlockingDTLS_Connect(CYASSL* ssl)
 	    error = 2;
 	}
 	else if (select_ret == TEST_TIMEOUT && CyaSSL_dtls(ssl) && 
-					CyaSSL_dtls_got_timeout(ssl) >= 0) {
+		CyaSSL_dtls_got_timeout(ssl) >= 0) {
 	    error = 2;
 	}
 	else{
@@ -93,7 +93,7 @@ static void NonBlockingDTLS_Connect(CYASSL* ssl)
     }
 
     if (ret != SSL_SUCCESS)
-        err_sys("SSL_connect failed with");
+	err_sys("SSL_connect failed with");
 }
 
 /*Main send and receive function*/
@@ -103,22 +103,21 @@ void DatagramClient (FILE* clientInput, CYASSL* ssl) {
     char    sendLine[MAXLINE], recvLine[MAXLINE - 1];
 
     fgets(sendLine, MAXLINE, clientInput);
-        
-       if ( ( CyaSSL_write(ssl, sendLine, strlen(sendLine))) != 
-	      strlen(sendLine)) {
-            err_sys("SSL_write failed");
-        }
 
-       n = CyaSSL_read(ssl, recvLine, sizeof(recvLine)-1);
-       
-       if (n < 0){
-            int readErr = CyaSSL_get_error(ssl, 0);
-	    if(readErr != SSL_ERROR_WANT_READ)
-		err_sys("CyaSSL_read failed");
-       }
+    while  ( ( CyaSSL_write(ssl, sendLine, strlen(sendLine))) != 
+	    strlen(sendLine)) {
+	err_sys("SSL_write failed");
+    }
 
-        recvLine[n] = '\0';  
-        fputs(recvLine, stdout);
+    while ( (n = CyaSSL_read(ssl, recvLine, sizeof(recvLine)-1)) <= 0) {
+
+	int readErr = CyaSSL_get_error(ssl, 0);
+	if(readErr != SSL_ERROR_WANT_READ)
+	    err_sys("CyaSSL_read failed");
+    }
+
+    recvLine[n] = '\0';  
+    fputs(recvLine, stdout);
 
 }
 
@@ -134,41 +133,41 @@ int main (int argc, char** argv) {
     int 		resumeSession = 0;
 
     if (argc != 2) {
-        perror("usage: udpcli <IP address>\n");
-        exit(1);
+	perror("usage: udpcli <IP address>\n");
+	exit(1);
     }
 
     signal(SIGINT, sig_handler);
 
     CyaSSL_Init();
-    CyaSSL_Debugging_ON();
-   
+    /*    CyaSSL_Debugging_ON();*/
+
     if ( (ctx = CyaSSL_CTX_new(CyaDTLSv1_2_client_method())) == NULL){
-        fprintf(stderr, "CyaSSL_CTX_new error.\n");
-        exit(EXIT_FAILURE);
+	fprintf(stderr, "CyaSSL_CTX_new error.\n");
+	exit(EXIT_FAILURE);
     }
 
     if (CyaSSL_CTX_load_verify_locations(ctx,"../cyassl/certs/ca-cert.pem",0) 
-	!= SSL_SUCCESS) {
-        fprintf(stderr, 
+	    != SSL_SUCCESS) {
+	fprintf(stderr, 
 		"Error loading ../certs/ca-cert.pem, please check the file.\n");
-        exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
     }
 
     ssl = CyaSSL_new(ctx);
     if (ssl == NULL)
-    	err_sys("unable to get ssl object");
-    
+	err_sys("unable to get ssl object");
+
     bzero(&servAddr, sizeof(servAddr));
     servAddr.sin_family = AF_INET;
     servAddr.sin_port = htons(SERV_PORT);
     inet_pton(AF_INET, host, &servAddr.sin_addr);
 
     CyaSSL_dtls_set_peer(ssl, &servAddr, sizeof(servAddr));
-    
+
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) 
-       err_sys("cannot create a socket."); 
-    
+	err_sys("cannot create a socket."); 
+
     CyaSSL_set_fd(ssl, sockfd);
     CyaSSL_set_using_nonblock(ssl, 1);
     fcntl(sockfd, F_SETFL, O_NONBLOCK);
@@ -176,7 +175,8 @@ int main (int argc, char** argv) {
 
     DatagramClient(stdin, ssl);
     char* srTest = "testing session resume";
-    CyaSSL_write(ssl, srTest, sizeof(srTest));
+    while ( (CyaSSL_write(ssl, srTest, sizeof(srTest))) != sizeof(srTest)) 
+	err_sys("failed to write");
     session = CyaSSL_get_session(ssl);
     sslResume = CyaSSL_new(ctx);
 
@@ -190,10 +190,10 @@ int main (int argc, char** argv) {
     inet_pton(AF_INET, host, &servAddr.sin_addr);
 
     CyaSSL_dtls_set_peer(sslResume, &servAddr, sizeof(servAddr));
-   
+
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) 
-       err_sys("cannot create a socket."); 
-    
+	err_sys("cannot create a socket."); 
+
     CyaSSL_set_fd(sslResume, sockfd);
     CyaSSL_set_session(sslResume, session);
     CyaSSL_set_using_nonblock(sslResume, 1);
@@ -201,12 +201,13 @@ int main (int argc, char** argv) {
     NonBlockingDTLS_Connect(sslResume);
 
     if(CyaSSL_session_reused(sslResume))
-    	printf("reused session id\n");
+	printf("reused session id\n");
     else
-    	printf("didn't reuse session id!!!\n");
-    
+	printf("didn't reuse session id!!!\n");
+
     DatagramClient(stdin, sslResume);
-    CyaSSL_write(sslResume, srTest, sizeof(srTest));
+    while ((CyaSSL_write(sslResume, srTest, sizeof(srTest))) != sizeof(srTest)) 
+	err_sys("failed to write");
     sleep(1);
 
     CyaSSL_shutdown(sslResume);
